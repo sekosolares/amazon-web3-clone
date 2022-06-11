0e8a2ee877bf1e2c00c0b72072d5e062d965630f
sekosolares <axel.solares.g@gmail.com>
2022-06-11 17:29:35 -0600

    Add TailwindCSS and other deps

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_API_URL}
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
 export default MyApp
