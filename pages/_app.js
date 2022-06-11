58f0156db40888b917e0eb6d47dd250704217bef
sekosolares <axel.solares.g@gmail.com>
2022-06-11 17:32:18 -0600

    Update corrected env variables

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider
-      serverUrl={process.env.NEXT_PUBLIC_API_URL}
-      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
     >
       <Component {...pageProps} />
     </MoralisProvider>
